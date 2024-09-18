2b7357d8acb1d850132b0c8f802dfb9d832b4760
ex3mpl <zevirikzefirka3@gmail.com>
2024-09-18 19:48:17 +0500

    kata pre project task 1.1.4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,10 +2,7 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,52 +15,56 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() throws SQLException {
-        connection.createStatement().execute(
-                """
-                        CREATE TABLE IF NOT EXISTS `userdao`.`users` (
-                                 `id` INT NOT NULL AUTO_INCREMENT,
-                                 `name` VARCHAR(45) NOT NULL,
-                                 `lastname` VARCHAR(45) NOT NULL,
-                                 `age` INT(3) NULL,
-                        PRIMARY KEY (`id`));
-                        """);
+        String command = """
+                CREATE TABLE IF NOT EXISTS `userdao`.`users` (
+                         `id` INT NOT NULL AUTO_INCREMENT,
+                         `name` VARCHAR(45) NOT NULL,
+                         `lastname` VARCHAR(45) NOT NULL,
+                         `age` INT(3) NULL,
+                PRIMARY KEY (`id`));
+                """;
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(command);
+        }
     }
 
     public void dropUsersTable() throws SQLException {
-        connection.createStatement().execute("DROP TABLE IF EXISTS `userdao`.`users`");
+        String command = "DROP TABLE IF EXISTS `userdao`.`users`";
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(command);
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) throws SQLException {
-        PreparedStatement statement = connection.prepareStatement(
-                "INSERT INTO `userdao`.`users` (name, lastname, age) " +
-                        "VALUES (?, ?, ?)"
-        );
-        statement.setString(1, name);
-        statement.setString(2, lastName);
-        statement.setByte(3, age);
-        statement.executeUpdate();
+        String command = "INSERT INTO `userdao`.`users` (name, lastname, age) VALUES (?, ?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(command)) {
+            statement.setString(1, name);
+            statement.setString(2, lastName);
+            statement.setByte(3, age);
+            statement.executeUpdate();
+        }
     }
 
     public void removeUserById(long id) throws SQLException {
-        PreparedStatement statement = connection.prepareStatement(
-                "DELETE FROM `userdao`.`users` WHERE `id` = ?;"
-        );
-        statement.setLong(1, id);
-        statement.executeUpdate();
+        String command = "DELETE FROM `userdao`.`users` WHERE `id` =?";
+        try (PreparedStatement statement = connection.prepareStatement(command)) {
+            statement.setLong(1, id);
+            statement.executeUpdate();
+        }
     }
 
     public List<User> getAllUsers() throws SQLException {
         List<User> users = new ArrayList<>();
         try (PreparedStatement statement = connection.prepareStatement(
-                "SELECT * FROM `userdao`.`users`")) {
+                "SELECT * FROM `userdao`.`users`" )) {
 
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
                 User user = new User();
-                user.setId(resultSet.getLong("id"));
-                user.setName(resultSet.getString("name"));
-                user.setLastName(resultSet.getString("lastname"));
-                user.setAge(resultSet.getByte("age"));
+                user.setId(resultSet.getLong("id" ));
+                user.setName(resultSet.getString("name" ));
+                user.setLastName(resultSet.getString("lastname" ));
+                user.setAge(resultSet.getByte("age" ));
                 users.add(user);
             }
         } catch (Exception e) {
@@ -73,6 +74,9 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() throws SQLException {
-        connection.createStatement().executeUpdate("TRUNCATE TABLE `userdao`.`users`");
+        String command = "TRUNCATE TABLE `userdao`.`users`";
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(command);
+        }
     }
 }
