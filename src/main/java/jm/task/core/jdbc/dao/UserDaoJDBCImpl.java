5e67f5eb575195efee859fc71d4bfd68a266f554
ex3mpl <zevirikzefirka3@gmail.com>
2024-09-18 21:12:45 +0500

    kata pre project task 1.1.4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -68,7 +68,7 @@ public class UserDaoJDBCImpl implements UserDao {
                 users.add(user);
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            e.getStackTrace();
         }
         return users;
     }
